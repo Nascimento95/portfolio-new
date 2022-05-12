efa31ef1b6660f5b66f6f9093117425d2a9b9c8d
Nascimento95 <marco-pereira@hotmail.fr>
2022-05-12 17:51:30 +0200

    feat: onglet js project fini

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-let section = document.querySelector(".test")
-// section.style.background = "blue"
+let section = document.querySelector(".my-projects")
 let titleProject = document.querySelector(".mes_projet")
-let containerProject = document.querySelector(".container-all-projects")
+let containerAllProject = document.querySelector(".container-all-projects")
 let projetPro = document.querySelector('.pro')
 let projetsPro = document.querySelector('.container-projects-pro')
 
 const changeColor = () => {
-    titleProject.style.color = "red"
+    section.style.height = "1750px"           
+    titleProject.style.color = "grey"
     projetsPro.style.visibility = "hidden"
-    containerProject.style.display = "display"
+    containerAllProject.style.display = "grid"
+    projetPro.style.color = "black"
 
 }
 
 titleProject.addEventListener("click",() => {
     console.log("sa click")
-    // containerProject.style.visibility = "visibility"
+    // containerAllProject.style.visibility = "visibility"
     changeColor()
 } )
 
 projetPro.addEventListener("click", () => {
     console.log("salut")
-    containerProject.style.display = "none"
+    projetPro.style.color = "grey"
+    titleProject.style.color = "black"
+    section.style.height = "700px"
+    containerAllProject.style.display = "none"
     projetsPro.style.visibility = "visible"
 })
