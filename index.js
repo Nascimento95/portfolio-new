fdb39dfd47016f5c5f66a3a784f401c3a38f7566
Nascimento95 <marco-pereira@hotmail.fr>
2022-05-12 09:40:55 +0200

    feat : onglet projet en cour de dev

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ let section = document.querySelector(".test")
 // section.style.background = "blue"
 let titleProject = document.querySelector(".mes_projet")
 let containerProject = document.querySelector(".container-all-projects")
+let projetPro = document.querySelector('.pro')
+let projetsPro = document.querySelector('.container-projects-pro')
 
 const changeColor = () => {
     titleProject.style.color = "red"
-    containerProject.style.visibility = "visibility"
+    projetsPro.style.visibility = "hidden"
+    containerProject.style.display = "display"
+
 }
 
 titleProject.addEventListener("click",() => {
     console.log("sa click")
+    // containerProject.style.visibility = "visibility"
     changeColor()
 } )
+
+projetPro.addEventListener("click", () => {
+    console.log("salut")
+    containerProject.style.display = "none"
+    projetsPro.style.visibility = "visible"
+})
