674ece212fce998dd48975ec73c2eb99d4c864a1
Nascimento95 <marco-pereira@hotmail.fr>
2022-05-17 15:22:59 +0200

    feat: emailjs and sweetalert

--- index.js
+++ index.js
@@ -4,8 +4,54 @@ let containerAllProject = document.querySelector(".container-all-projects")
 let projetPro = document.querySelector('.pro')
 let projetsPro = document.querySelector('.container-projects-pro')
 
+let btnSubmit = document.querySelector(".btn-sub")
+
+let whidthEcran;
+
+
+// const successSubmit =(e) => {
+//     e.preventDefault()
+//     Swal.fire({
+//         position: 'top-end',
+//         icon: 'success',
+//         title: 'Your work has been saved',
+//         showConfirmButton: false,
+//         timer: 1500
+//     })
+// }
+btnSubmit.addEventListener("click",(e) => successSubmit )
+
+// let checkTheSizeWindow =  setInterval(() => {
+//     whidthEcran = document.documentElement.scrollWidth
+//     console.log(" ecran oui oui ",whidthEcran)
+//     if(whidthEcran > 720){
+//         console.log("dans la condi > 720");
+//         section.style.height = "720px"
+//     }
+//     if(whidthEcran > 100 && whidthEcran < 619) {
+//         // clearInterval(checkTheSizeWindow)
+//         console.log("dans la condi 100 a 619");
+//         section.style.height = "1750px"           
+//     }
+//     if(whidthEcran > 619 && whidthEcran < 939) {
+//         console.log("dans la condi 619 a 939");
+//         section.style.height = "1020px"           
+//     }
+// }, 1000);
+
 const changeColor = () => {
-    section.style.height = "1750px"           
+    if(whidthEcran > 720){
+        section.style.height = "720px"
+    }
+    if(whidthEcran > 100 && whidthEcran < 619) {
+        clearInterval(checkTheSizeWindow)
+        console.log("dans la condi");
+        section.style.height = "1750px"           
+    }
+    if(whidthEcran > 619 && whidthEcran < 939) {
+        console.log("dans la condi");
+        section.style.height = "1020px"           
+    }
     titleProject.style.color = "grey"
     projetsPro.style.visibility = "hidden"
     containerAllProject.style.display = "grid"
@@ -21,6 +67,7 @@ titleProject.addEventListener("click",() => {
 
 projetPro.addEventListener("click", () => {
     console.log("salut")
+    clearInterval(checkTheSizeWindow)
     projetPro.style.color = "grey"
     titleProject.style.color = "black"
     section.style.height = "700px"
