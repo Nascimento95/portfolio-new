059d5cb7d8d26e9ea842d0b94517e3737332c390
Nascimento95 <marco-pereira@hotmail.fr>
2022-05-23 15:22:33 +0200

    feat: mail js and download cv

--- index.js
+++ index.js
@@ -4,43 +4,33 @@ let containerAllProject = document.querySelector(".container-all-projects")
 let projetPro = document.querySelector('.pro')
 let projetsPro = document.querySelector('.container-projects-pro')
 
-let btnSubmit = document.querySelector(".btn-sub")
-
 let whidthEcran;
 
 
-// const successSubmit =(e) => {
-//     e.preventDefault()
-//     Swal.fire({
-//         position: 'top-end',
-//         icon: 'success',
-//         title: 'Your work has been saved',
-//         showConfirmButton: false,
-//         timer: 1500
-//     })
-// }
-btnSubmit.addEventListener("click",(e) => successSubmit )
+let checkTheSizeWindow = () =>{
+    setInterval(() => {
+        whidthEcran = document.documentElement.scrollWidth
+        console.log(" ecran oui oui ",whidthEcran)
+        if(whidthEcran > 720){
+            console.log("dans la condi > 720");
+            section.style.height = "720px"
+        }
+        if(whidthEcran > 100 && whidthEcran < 619) {
+            // clearInterval(checkTheSizeWindow)
+            console.log("dans la condi 100 a 619");
+            section.style.height = "1750px"           
+        }
+        if(whidthEcran > 619 && whidthEcran < 939) {
+            console.log("dans la condi 619 a 939");
+            section.style.height = "1020px"           
+        }
+    }, 1000);
+}
 
-// let checkTheSizeWindow =  setInterval(() => {
-//     whidthEcran = document.documentElement.scrollWidth
-//     console.log(" ecran oui oui ",whidthEcran)
-//     if(whidthEcran > 720){
-//         console.log("dans la condi > 720");
-//         section.style.height = "720px"
-//     }
-//     if(whidthEcran > 100 && whidthEcran < 619) {
-//         // clearInterval(checkTheSizeWindow)
-//         console.log("dans la condi 100 a 619");
-//         section.style.height = "1750px"           
-//     }
-//     if(whidthEcran > 619 && whidthEcran < 939) {
-//         console.log("dans la condi 619 a 939");
-//         section.style.height = "1020px"           
-//     }
-// }, 1000);
 
 const changeColor = () => {
     if(whidthEcran > 720){
+        clearInterval(checkTheSizeWindow)
         section.style.height = "720px"
     }
     if(whidthEcran > 100 && whidthEcran < 619) {
@@ -49,6 +39,7 @@ const changeColor = () => {
         section.style.height = "1750px"           
     }
     if(whidthEcran > 619 && whidthEcran < 939) {
+        clearInterval(checkTheSizeWindow)
         console.log("dans la condi");
         section.style.height = "1020px"           
     }
@@ -61,12 +52,14 @@ const changeColor = () => {
 
 titleProject.addEventListener("click",() => {
     console.log("sa click")
+    // checkTheSizeWindow()
     // containerAllProject.style.visibility = "visibility"
     changeColor()
 } )
 
 projetPro.addEventListener("click", () => {
     console.log("salut")
+    // checkTheSizeWindow()
     clearInterval(checkTheSizeWindow)
     projetPro.style.color = "grey"
     titleProject.style.color = "black"
